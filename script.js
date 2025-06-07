41667b5eaa0bc41ab9be63770b7a236ae39fb5ed
kunc-and-snake <104681984+kunc-and-snake@users.noreply.github.com>
2025-06-07 23:04:07 +0200

    Source Files

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleNavbarHamburgerMenu() {
+    let menu = document.getElementById("navbar-hamburger-menu");
+    menu.style.display = menu.style.display === "" ? "flex" : "";
+    
+      
+}
